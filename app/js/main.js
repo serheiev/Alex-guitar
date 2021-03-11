dbfdc65107c347ffc74c9c900f7faee884a47ded
unknown <fragshell@yandex.ru>
2021-03-11 20:34:55 +0200

    on work

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+  $(document).ready(function() {
+    $('.burger-icon, .menu-item').click(function() {
+      $('.burger-icon').toggleClass("change");
+      $('.header-menu').toggleClass("pushed");
+    })
+    
+
+});
