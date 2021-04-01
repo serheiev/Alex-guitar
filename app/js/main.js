4221af2aa41d8ee74927f4e310f0f806440297ef
serheiev <69833095+serheiev@users.noreply.github.com>
2021-04-01 20:46:09 +0300

    fixed media query and other minor edits

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,11 @@
       $('.burger-icon').toggleClass("change");
       $('.header-menu').toggleClass("pushed");
     })
-    
+    $("#menu").on("click","a", function (event) {
+      event.preventDefault(); //опустошим стандартную обработку
+      var id  = $(this).attr('href'), //заберем айдишник блока с параметром URL
+        top = $(id).offset().top; //определим высоту от начала страницы до якоря
+    $('body,html').animate({scrollTop: top}, 1000); //сделаем прокрутку за 1 с
+    });
 
 });
